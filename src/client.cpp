2d83ec48f90298edb5e5c4faf5e715fc7b14c70e
Arian Useini <auseini20@gmail.com>
2023-11-23 13:29:46 -0600

    update cmake config

---
+++ src/client.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <string>
+#include <thread>
+int main() {
+    std::string userName;
+    std::getline(std::cin, userName);
+    std::cout << userName << std::endl;
+    std::cout << "ITS LIKE WHO DID I ASK" << std::endl;
+
+//    int socket = socket(AF_INET, SOCK_STREAM, 0);
+
+
+    // bind my socket
+    //
+    // create send and recieve threads
+    //
+
+  return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main(){
+    std::cout << "i am ther server" << std::endl;
+    return 0;
+}
