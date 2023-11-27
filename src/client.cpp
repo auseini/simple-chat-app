0acbbb4d02e80f60e1afc3ca0cf8182df809fd55
Arian Useini <auseini20@gmail.com>
2023-11-26 20:02:42 -0600

    basic 1 user chat

--- src/client.cpp
+++ src/client.cpp
@@ -2,19 +2,71 @@
 #include <sys/socket.h>
 #include <string>
 #include <thread>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+
+void readFromSocket(int socket);
+void writeToSocket(int socket);
+
+
 int main() {
     std::string userName;
     std::getline(std::cin, userName);
     std::cout << userName << std::endl;
-    std::cout << "ITS LIKE WHO DID I ASK" << std::endl;
+    char buffer[1024] = {0};
+    int client = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serverAddress;  
+    if(client < 0) {
+        std::cerr << "failed to open socket" << std::endl;;
+        return 1;
+    }
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(PORT);
 
-//    int socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(inet_pton(AF_INET,"127.0.0.1", &serverAddress.sin_addr) <= 0){
+        std::cerr << "address is invalid" << std::endl;
+        return EXIT_FAILURE;
+    }
 
+    std::cout << "Connecting to server..." << std::endl;
+    if(connect(client, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0){
+        std::cerr << "failed to connect to server" << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << "Connected." << std::endl;
+    send(client, userName.c_str(), strlen(userName.c_str()) ,0); 
 
+    std::thread readThread(readFromSocket, client);
+    std::thread writeThread(writeToSocket, client);
+
+    readThread.join();
+    writeThread.join();
     // bind my socket
     //
     // create send and recieve threads
     //
+    close(client);
 
-  return 0;
+     return 0;
+}
+
+
+void readFromSocket(int socket){
+    char line[1024] = {0};
+    while(true){
+        read(socket, line, 1024 - 1);
+        std::cout << line << std::endl;
+        memset(line, 0, sizeof(line));
+    }
+}
+void writeToSocket(int socket){
+    std::string line;
+    while(true){
+        getline(std::cin, line);
+        send(socket, line.c_str(), strlen(line.c_str()), 0);
+        std::cin.clear();
+        std::cout << "\033[A";
+    }
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,67 @@
+#include <functional>
 #include <iostream>
-
+#include <sstream>
+#include <sys/_types/_socklen_t.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <thread>
+#define PORT 8080
 
 int main(){
+    int server;
     std::cout << "i am ther server" << std::endl;
+
+    server = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+    int opt = 1;
+
+    if(server < 0){
+        std::cerr << "failed to create socket" << std::endl;
+        return 1;
+    }
+   
+  //  if(setsockopt(server, SOL_SOCKET, SO_REUSEPORT | SO_REUSEADDR, &opt, sizeof(opt))){
+  //      std::cerr << "failed to set socket options" << std::endl;
+  //      return EXIT_FAILURE;
+  //  }
+    
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+
+    if(bind(server,(struct sockaddr*)&address, addrlen) < 0){
+        std::cerr << "failed to bind socket" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if(listen(server,10) < 0 ){
+        std::cerr << "listen failure" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "done listening" << std::endl;
+
+    //loop forever, and check for a new connection
+    //accept connection, spawn nee thread with that 
+    int newSocket = accept(server, (struct sockaddr*) &address, &addrlen);
+    std::cout << "wow a new connection" << std::endl;
+    
+    char line[1024];
+    std::string name;
+    name.resize(1024);
+    read(newSocket, &name[0], 1024-1);
+    while(true){
+        read(newSocket, line, 1024 - 1);
+        std::cout << "sending: " << line << std::endl;
+        std::stringstream toSend;
+        toSend << &name[0] << ": " << line;
+        send(newSocket, toSend.str().c_str(), strlen(toSend.str().c_str()), 0);
+        memset(line, 0, sizeof(line));
+    }
+
+    close(newSocket);
+    close(server);
     return 0;
 }
